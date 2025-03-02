a5d402e9e99e2c04fdf5eaf2d935aacb4b4f10d4
Thomas GOMES <thomasgomes2005.94@gmail.com>
2025-03-02 14:38:40 +0100

    ajout modal validation de la supression

--- src/app/components/voyagepage/voyagepage.component.ts
+++ src/app/components/voyagepage/voyagepage.component.ts
@@ -1,39 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {VoyageService} from "../../services/voyage.service";
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { VoyageService } from '../../services/voyage.service';
 
 @Component({
   selector: 'app-voyagepage',
   standalone: true,
   imports: [],
   templateUrl: './voyagepage.component.html',
-  styleUrl: './voyagepage.component.css'
+  styleUrls: ['./voyagepage.component.css']
 })
-
-export class VoyagepageComponent implements OnInit{
+export class VoyagepageComponent implements OnInit {
   destination!: string;
   description!: string;
   prix!: number;
   id!: number;
+  isModalOpen: boolean = false;
 
-  constructor(private route: ActivatedRoute,private router: Router, private voyageservice: VoyageService) {
-  }
+  constructor(private route: ActivatedRoute, private router: Router, private voyageservice: VoyageService) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params =>{
+    this.route.paramMap.subscribe(params => {
       this.id = Number(params.get('id'));
-      if(!isNaN(this.id)){
+      if (!isNaN(this.id)) {
         let voyage = this.voyageservice.getVoyageById(this.id);
-        if(voyage) {
+        if (voyage) {
           this.destination = voyage.destination;
           this.description = voyage.description;
           this.prix = voyage.prix;
         }
       }
-    })
+    });
   }
 
-  onSuprimmer(){
-    this.router.navigate(['listevoyages']).then(r => this.voyageservice.suprimmerVoyage(this.id));
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  confirmSuprimmer() {
+    this.voyageservice.suprimmerVoyage(this.id);
+    this.router.navigate(['listevoyages']).then(() => {
+      this.closeModal();
+    });
   }
 }
